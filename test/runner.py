33b1553add1d0cab99721505c17d2c8dcf67c5dd
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2012-12-10 00:10:45 +0100

    Rename the nagios-results test suite into a valid identifier.
    This way, we can run its tests from within a test.runner module.

--- test/runner.py
+++ test/runner.py
@@ -1,12 +1,14 @@
+# -*- encoding: utf-8 -*-
 import sys
 import os
 import test.cache as tc
 import test.dateandtime as td
 import test.nagios as tn
 import test.generaloption as tg
+import test.nagios_results as tr
 import unittest
 
-suite = unittest.TestSuite([x.suite() for  x in (tc, td, tn, tg)])
+suite = unittest.TestSuite([x.suite() for  x in (tc, td, tn, tg, tr)])
 
 try:
     import xmlrunner
