b5f5465f07e0dee56a41039e294c644b78942de2
stdweird <stijn.deweirdt@ugent.be>
2013-03-09 17:39:36 +0100

    add get_options_by_section to generaloption

--- lib/vsc/utils/generaloption.py
+++ lib/vsc/utils/generaloption.py
@@ -924,6 +924,10 @@ class GeneralOption(object):
             prefix_dict[new_dest] = value
         return prefix_dict
 
+    def get_options_by_section(self, section):
+        """Get all options from section. Return a dict."""
+        return self._get_options_by_property('section_name', section).items()
+
     def postprocess(self):
         """Some additional processing"""
         pass
