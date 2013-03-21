32cfebb65a972941a954ef2ece10912f08820f3e
stdweird <stijn.deweirdt@ugent.be>
2013-03-21 23:05:58 +0100

    introduce run_timeout and simple unittests for run

--- test/runner.py
+++ test/runner.py
@@ -1,18 +1,23 @@
 # -*- encoding: utf-8 -*-
-import sys
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
 import test.cache as tc
 import test.dateandtime as td
 import test.nagios as tn
 import test.generaloption as tg
 import test.nagios_results as tr
 import test.fancylogger as tf
+import test.run as trun
 import unittest
+
 
 from vsc.utils import fancylogger
 fancylogger.logToScreen(enable=False)
 
-suite = unittest.TestSuite([x.suite() for  x in (tc, td, tn, tg, tr, tf)])
+suite = unittest.TestSuite([x.suite() for  x in (tc, td, tn, tg, tr, tf, trun)])
 
 try:
     import xmlrunner
