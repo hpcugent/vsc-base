306595bed196189ae1d9c6cfc5a03027729d5f4d
Andy Georges <itkovian@gmail.com>
2013-03-28 16:05:58 +0100

    version bump

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from shared_setup import action_target
 
 PACKAGE = {
     'name': 'vsc-base',
-    'version': '1.2',
+    'version': '1.3',
     'author': [sdw, jt, ag],
     'maintainer': [sdw, jt, ag],
     'packages': ['vsc', 'vsc.utils'],
