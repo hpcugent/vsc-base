9dc89398af21ed34987e2baefed6d53908194b00
Andy Georges <itkovian@gmail.com>
2013-04-18 18:05:09 +0200

    Removed lockfile dependency

--- setup.py
+++ setup.py
@@ -51,14 +51,13 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-base',
-    'version': '1.4.1',
+    'version': '1.4.2',
     'author': [sdw, jt, ag],
     'maintainer': [sdw, jt, ag],
     'packages': ['vsc', 'vsc.utils', 'vsc.install'],
     'provides': ['python-vsc-packages-common = 0.5',
                  'python-vsc-packages-logging = 0.14',
                  'python-vsc-packages-utils = 0.11'],
-    'install_requires': ['lockfile >= 0.9.1'],
     'scripts': ['bin/logdaemon.py', 'bin/startlogdaemon.sh', 'bin/bdist_rpm.sh'],
 }
 
