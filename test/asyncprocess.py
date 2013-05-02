7bf50e2fba668a9a994901187f3017f2df423a5f
stdweird <stijn.deweirdt@ugent.be>
2013-05-02 07:10:59 +0200

    fix headers in asyncprocess unittest

--- test/asyncprocess.py
+++ test/asyncprocess.py
@@ -1,31 +1,33 @@
 # #
 # Copyright 2012-2013 Ghent University
 #
-# This file is part of EasyBuild,
+# This file is part of vsc-base,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
 # with support of Ghent University (http://ugent.be/hpc),
 # the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
 # the Hercules foundation (http://www.herculesstichting.be/in_English)
 # and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
 #
-# http://github.com/hpcugent/easybuild
+# http://github.com/hpcugent/vsc-base
 #
-# EasyBuild is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation v2.
+# vsc-base is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Library General Public License as
+# published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
 #
-# EasyBuild is distributed in the hope that it will be useful,
+# vsc-base is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Library General Public License
+# along with vsc-base. If not, see <http://www.gnu.org/licenses/>.
 # #
 """
 Unit tests for asyncprocess.py.
 
 @author: Toon Willems (Ghent University)
+@author: Stijn De Weirdt (Ghent University)
 """
 
 import os
@@ -69,9 +71,11 @@ class AsyncProcessTest(TestCase):
         """cleanup"""
         os.chdir(self.cwd)
 
+
 def suite():
     """ returns all the testcases in this module """
     return TestSuite([AsyncProcessTest()])
+
 
 if __name__ == '__main__':
     main()
