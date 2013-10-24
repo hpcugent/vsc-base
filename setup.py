b5ee221d0c4930d666c9362daa63030b0b6eea5e
Jens Timmerman <jens.timmerman@ugent.be>
2013-10-24 14:43:41 +0200

    make raw sections less acident prone

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-base',
-    'version': '1.6.5',
+    'version': '1.6.6',
     'author': [sdw, jt, ag],
     'maintainer': [sdw, jt, ag],
     'packages': ['vsc', 'vsc.utils', 'vsc.install'],
