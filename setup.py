e1cc688985f91bc993ac9fff3ba703639bb45f23
stdweird <stijn.deweirdt@ugent.be>
2013-11-07 19:46:01 +0100

    bump version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-base',
-    'version': '1.6.6',
+    'version': '1.6.7',
     'author': [sdw, jt, ag],
     'maintainer': [sdw, jt, ag],
     'packages': ['vsc', 'vsc.utils', 'vsc.install'],
