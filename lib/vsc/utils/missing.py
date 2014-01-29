2562f13e1804d78005e21beeb80e9aa91e1946b6
Andy Georges <itkovian@gmail.com>
2014-01-29 14:59:22 +0100

    topological sorting. of stuff.

--- lib/vsc/utils/missing.py
+++ lib/vsc/utils/missing.py
@@ -265,3 +265,28 @@ class TryOrFail(object):
                         time.sleep(self.sleep)
 
         return new_function
+
+
+def post_order(graph, root):
+    """
+    Walk the graph from the given root in a post-order manner by providing the corresponding generator
+    """
+    for node in graph[root]:
+        for child in post_order(graph, node):
+            yield child
+        yield node
+    yield root
+
+
+def topological_sort(graph, root):
+    """
+    Perform topological sorting of the given graph at the given root.
+
+    The graph is a dict with the values for a key being the dependencies, i.e., an arrow from key to each value.
+    """
+    visited = set()
+    for node in post_order(graph, root):
+        if not node in visited:
+            yield node
+            visited.add(node)
+
