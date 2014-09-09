da623afc54c3d4bfb4d2f218f320edddb286cee3
stdweird <stijn.deweirdt@ugent.be>
2014-09-09 13:47:59 +0200

    run the generate_cmd_line with logaction

--- test/generaloption.py
+++ test/generaloption.py
@@ -155,11 +155,12 @@ class GeneralOptionTest(TestCase):
                                     '--level-prefix-and-dash=YY',
                                     shell_unquote('--store="some whitespace"'),
                                     '--ext-strlist=x,y',
+                                    '--debug',
                                     ])
         self.assertEqual(topt.options.__dict__,
                          {
                           'store': 'some whitespace',
-                          'debug': False,
+                          'debug': True,
                           'info':False,
                           'quiet':False,
                           'level_level': True,
