72439ebaab394a0b2a31a6be024fd3214b207fd3
stdweird <stijn.deweirdt@ugent.be>
2015-05-11 06:50:30 +0200

    bump version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-base',
-    'version': '2.2.0',
+    'version': '2.2.1',
     'author': [sdw, jt, ag, kh],
     'maintainer': [sdw, jt, ag, kh],
     'packages': ['vsc', 'vsc.utils', 'vsc.install'],
