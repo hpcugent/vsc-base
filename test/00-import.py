46e8bca3dd8e5966d1deb62e0b1a4f2ce1a99bfa
Jens Timmerman <jens.timmerman@ugent.be>
2016-05-18 13:12:36 +0200

    fixed headers

--- test/00-import.py
+++ test/00-import.py
@@ -4,7 +4,7 @@
 # This file is part of vsc-base,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
 # with support of Ghent University (http://ugent.be/hpc),
-# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
 # the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
 # and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
 #
@@ -22,5 +22,6 @@
 #
 # You should have received a copy of the GNU Library General Public License
 # along with vsc-base. If not, see <http://www.gnu.org/licenses/>.
+#
 #
 from vsc.install.commontest import CommonTest
