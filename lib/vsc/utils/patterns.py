7d5a80ebdf73050684ce9367769c2f6771c6dbbd
stdweird <stijn.deweirdt@ugent.be>
2018-03-01 09:38:36 +0100

    pattterns: Singleton: support skip_singleton named arg to bypass Singleton metaclass

--- lib/vsc/utils/patterns.py
+++ lib/vsc/utils/patterns.py
@@ -46,6 +46,12 @@ class Singleton(type):
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+        def init():
+            return super(Singleton, cls).__call__(*args, **kwargs)
+
+        if kwargs.pop('skip_singleton', False):
+            return init()
+        else:
+            if cls not in cls._instances:
+                cls._instances[cls] = init()
+            return cls._instances[cls]
