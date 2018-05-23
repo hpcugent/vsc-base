2608769b62759ad4016a3d4e875b13c6c48bfa9b
Andy Georges <itkovian@gmail.com>
2018-05-23 15:18:41 +0200

    Version bump to 0.10.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ vsc-base base distribution setup.py
 import vsc.install.shared_setup as shared_setup
 from vsc.install.shared_setup import ag, kh, jt, sdw
 
-VSC_INSTALL_REQ_VERSION = '0.10.1'
+VSC_INSTALL_REQ_VERSION = '0.10.2'
 
 _coloredlogs_pkgs = [
     'coloredlogs<6.0',     # automatic log colorizer
