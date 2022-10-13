1b9ff71f551fd877f7673fb4f99f5228e315ecf0
sam <sammoors@gmail.com>
2022-10-13 15:37:26 +0200

    only connect if mail host is not specified

--- lib/vsc/utils/mail.py
+++ lib/vsc/utils/mail.py
@@ -115,6 +115,8 @@ class VscMail(object):
             context = ssl.create_default_context()
             s.starttls(context=context)
             logging.debug("Started TLS connection")
+        elif not self.mail_host:
+            s.connect()
 
         if self.smtp_auth_user and self.smtp_auth_password:
             s.login(user=self.smtp_auth_user, password=self.smtp_auth_password)
