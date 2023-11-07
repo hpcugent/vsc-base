b3ef9e259c3957088aac21c0f2c8aab433220097
Wouter Depypere <wouter.depypere@ugent.be>
2023-11-07 09:59:30 +0100

    fix common parts

--- test/fancylogger.py
+++ test/fancylogger.py
@@ -532,14 +532,16 @@ class FancyLoggerTest(TestCase):
         msg = 'this is my string'
         logging.debug(msg)
 
+
+
         if sys.version_info < (3,7):
             # logging stream handling was changed in python 3.7
             # this is a good thing as messages can no longer go missing
-            self.assertEqual(stringfile.getvalue(), '',
-                             msg="logging.debug reports nothing when fancylogger loglevel is debug")
+            expected_msg = ''
         else:
-            self.assertEqual(stringfile.getvalue(), f'DEBUG:root:{msg}\n',
-                             msg="logging.debug reports something when fancylogger loglevel is debug")
+            expected_msg = f'DEBUG:root:{msg}\n'
+        self.assertEqual(stringfile.getvalue(), expected_msg,
+                         msg="logging.debug reports expected message (or not) when fancylogger loglevel is debug")
 
         fancylogger.setroot()
         self.assertTrue(isinstance(logging.root, fancylogger.FancyLogger),
