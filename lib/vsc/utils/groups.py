ccb0d257cee75969c278bae9868ff1cabb0ed7c9
Andy Georges <itkovian@gmail.com>
2025-10-14 19:27:09 +0200

    fix: should look for c, as in libc.a

--- lib/vsc/utils/groups.py
+++ lib/vsc/utils/groups.py
@@ -40,7 +40,7 @@ def getgrouplist(user, groupnames=True):
     returns a list of groupnames
     if groupames is false, returns a list of groupids (skip groupname lookups)
     """
-    libc = cdll.LoadLibrary(find_library('libc'))
+    libc = cdll.LoadLibrary(find_library('c'))
 
     ngetgrouplist = libc.getgrouplist
     # max of 50 groups should be enough as first try
